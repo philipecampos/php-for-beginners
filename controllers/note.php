336d09a938f302f3a7aef1b164f91ef5314d8c69
Philipe Barra <philipebarra@gmail.com>
2023-02-13 20:10:09 -0300

    Render the Notes and Note Page

---
+++ controllers/note.php
@@ -0,0 +1,9 @@
+<?php
+$config = require('config.php');
+$db = new Database($config['database']);
+
+$heading = 'My Note';
+
+$note = $db->query('SELECT * FROM notes where id = :id', ['id' => $_GET['id']])->fetch();
+
+require "views/note.view.php";
--- router.php
+++ router.php
@@ -4,6 +4,8 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $routes = [
     '/' => 'controllers/index.php',
     '/about' => 'controllers/about.php',
+    '/notes' => 'controllers/notes.php',
+    '/note' => 'controllers/note.php',
     '/contact' => 'controllers/contact.php',
 ];
 
@@ -12,6 +14,7 @@ function routeToController($uri, $routes)
     if (array_key_exists($uri, $routes)) {
         require $routes[$uri];
     } else {
+        dd($routes[$uri]);
         abort();
     }
 }
