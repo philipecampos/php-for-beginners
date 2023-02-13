7de9eedf010ba919ead41bd31fa0053856009943
Philipe Barra <philipebarra@gmail.com>
2023-02-13 20:36:39 -0300

    Introduction to Authorization

--- controllers/note.php
+++ controllers/note.php
@@ -3,7 +3,18 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = 'My Note';
+$currentUserId = 1;
 
-$note = $db->query('SELECT * FROM notes where id = :id', ['id' => $_GET['id']])->fetch();
+$note = $db->query('SELECT * FROM notes where id = :id', [
+    'id' => $_GET['id']
+])->fetch();
+
+if (!$note) {
+    abort();
+}
+
+if ($note['user_id'] !== $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
 
 require "views/note.view.php";
